7d69ac8874275d3cada5c66050f7bfabc8d208cb
johannpa <johann_paque@hotmail.fr>
2023-08-30 18:50:12 +0200

    Asynchronous calls in CharacterController, CharacterService and ICharacterService

--- dotnet-rpg/Services/CharacterService/CharacterService.cs
+++ dotnet-rpg/Services/CharacterService/CharacterService.cs
@@ -1,6 +1,7 @@
 ﻿using dotnet_rpg.Models;
 using System.Collections.Generic;
 using System.Linq;
+using System.Threading.Tasks;
 
 namespace dotnet_rpg.Services.CharacterService
 {
@@ -13,18 +14,18 @@ namespace dotnet_rpg.Services.CharacterService
         };
 
 
-        public List<Character> AddCharacter(Character newCharacter)
+        public async Task<List<Character>> AddCharacter(Character newCharacter)
         {
             characters.Add(newCharacter);
             return characters;
         }
 
-        public List<Character> GetAllCharacters()
+        public async Task<List<Character>> GetAllCharacters()
         {
             return characters;
         }
 
-        public Character GetCharacterById(int id)
+        public async Task<Character> GetCharacterById(int id)
         {
             return characters.FirstOrDefault(c => c.Id == id);
         }
--- dotnet-rpg/Services/CharacterService/ICharacterService.cs
+++ dotnet-rpg/Services/CharacterService/ICharacterService.cs
@@ -1,12 +1,13 @@
 ﻿using dotnet_rpg.Models;
 using System.Collections.Generic;
+using System.Threading.Tasks;
 
 namespace dotnet_rpg.Services.CharacterService
 {
     public interface ICharacterService
     {
-        List<Character> GetAllCharacters();
-        Character GetCharacterById(int id);
-        List<Character> AddCharacter(Character newCharacter);
+        Task<List<Character>> GetAllCharacters();
+        Task<Character> GetCharacterById(int id);
+        Task<List<Character>> AddCharacter(Character newCharacter);
     }
 }
